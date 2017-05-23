a28d2fcf0041612d03a42f73f171c8aed1c514b1
Jonathan Martin <jonmartin721@gmail.com>
2017-05-22 21:09:58 -0500

    Initial creation and documentation addition.

--- Number Puzzle/Number Puzzle.cpp
+++ Number Puzzle/Number Puzzle.cpp
@@ -3,9 +3,32 @@
 
 #include "stdafx.h"
 
+//154152 can be split evenly into two - digit pieces(15, 41, 52) – increasing order
+//154152 can be split evenly into three - digit pieces(154, 152) – not increasing order
+//154152 cannot be split evenly into five - digit pieces
+//
+//173 can be split evenly into one - digit pieces(1, 7, 3) – not increasing order
+//173 can be split evenly into three - digit pieces(173) – increasing order
+//173 cannot be split evenly into two - digit pieces
 
 int main()
 {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
 
